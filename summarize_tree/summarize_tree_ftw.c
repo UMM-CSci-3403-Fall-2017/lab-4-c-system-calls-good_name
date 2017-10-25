037993741f008aff285746a891a0e2a6551fda0b
smil1201 <mill5978@morris.umn.edu>
2017-10-24 23:35:43 -0500

    Cleaned and commented code.

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -13,6 +13,7 @@ static int num_dirs;
 static int num_regulars;
 
 static int callback(const char *fpath, const struct stat *sb, int typeflag){
+	//check type of arg, increment appropriate count	
 	if(typeflag == FTW_D) {
 		num_dirs++;
 	}else{
@@ -25,17 +26,16 @@ int main (int argc, char** argv){
 	num_dirs = 0;
 	num_regulars = 0;
 
-	  // Ensure an argument was provided.
-	 if (argc != 2) {
+	// Ensure an argument was provided.
+	if (argc != 2) {
 	    printf ("Usage: %s <path>\n", argv[0]);
-	    prinhttps://github.com/UMM-CSci-3403-Fall-2017/lab-4-c-system-calls-good_name.gittf ("       where <path> is the file or root of the tree you want to summarize.\n");
-
    	    return 1;
   }
 
-
+	// call subfunction
 	ftw(argv[1], callback, MAX_FTW_DEPTH);
-
+	
+	//print counts
 	printf("There were %d directories.\n", num_dirs);
 	printf("There were %d regular files.\n", num_regulars);
 
